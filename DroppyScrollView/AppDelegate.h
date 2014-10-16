56ab0b58d8507860d9764d1a43f60cd46cf7755b
cemolcay <ccemolcay@gmail.com>
2014-10-16 23:33:07 +0300

    Initial Commit

---
+++ DroppyScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DroppyScrollView
+//
+//  Created by Cem Olcay on 16/10/14.
+//  Copyright (c) 2014 questa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
